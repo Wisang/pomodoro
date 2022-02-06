c29d87b3980af5af7261a5a667b134899cff7ca7
wisang <wseom7@gmail.com>
2022-02-06 23:08:03 +0900

    timer generated

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from itertools import count
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -12,9 +13,17 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(300)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def count_down(count):
+    formatted_count = f"{int(count/60)}:{int(count%60)}"
+    canvas.itemconfig(timer_text, text=formatted_count)
+    if count > 0:
+        window.after(1000, count_down, count-1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -22,23 +31,23 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-head_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50, "bold"))
+head_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
 head_label.grid(column=1, row=0)
 
-check_label = Label(text="v", fg=GREEN, font=(FONT_NAME, 30, "bold"))
+check_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"))
 check_label.config(pady=20)
 check_label.grid(column=1, row=2)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, fill="white", font=(FONT_NAME, 35, "bold"), text="00:00")
+timer_text = canvas.create_text(100, 130, fill="white", font=(FONT_NAME, 35, "bold"), text="00:00")
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start", font=(FONT_NAME, 20, "bold"))
+start_button = Button(text="Start", font=(FONT_NAME, 15, "bold"), command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", font=(FONT_NAME, 20, "bold"))
+reset_button = Button(text="Reset", font=(FONT_NAME, 15, "bold"))
 reset_button.grid(column=2, row=2)
 
 
